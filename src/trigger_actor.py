70bf710663e8e57a24e5b1a6ef239a8b5f238ebb
Christian Cabello <christian.cabello@ip-paris.fr>
2025-06-16 14:34:25 +0200

    Typo fix

--- src/trigger_actor.py
+++ src/trigger_actor.py
@@ -11,13 +11,11 @@ ports = find_arduino_ports()
 pins = {"Pin2": 0, "Pin3": 1, "Pin4": 2}
 
 class ArduinoActor(Actor):
-    def __init__(self, name: str, host: str, port: int, device_info: dict, publisher_name: str, proxy_address: str, proxy_port: int, **kwargs):
+    def __init__(self, name: str, device_info: dict, publisher_name: str, proxy_address: str, proxy_port: int, **kwargs):
         super().__init__(name=name, device_class=ArduinoTrigger, **kwargs)
         self.connect(device_info=device_info, publisher_name=publisher_name, proxy_address=proxy_address, proxy_port=proxy_port)
         self.device_port = device_info['port']
         self.pins = device_info['pins']
-        self.host = host
-        self.port = port
 
         # Register functions for remote calls
         self.register_device_method(self.device.createRisingEdge)
